42834aa4fc21473ba3ba6870d5250e3785059355
vyinie <marcusvyinicius@gmail.com>
2023-10-05 15:08:02 -0300

    testes2

--- mhs-back/src/controllers/getControllers.ts
+++ mhs-back/src/controllers/getControllers.ts
@@ -155,7 +155,7 @@ export class Get {
         );
         if (i > moment()) {
           reservHours.push({
-            number: i.toISOString(),
+            number: i.utcOffset('+0300').toISOString(),
             available: isAvailable,
           });
         }
