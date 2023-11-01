bddbdcce455b406359908601c89717e554ddd6ef
Marcus Vinicius <110700931+vyinie@users.noreply.github.com>
2023-11-01 17:14:34 -0300

    deletei o link do whatsapp

--- mhs-front/components/mainPage/components/contact.tsx
+++ mhs-front/components/mainPage/components/contact.tsx
@@ -1,16 +1,9 @@
 "use client";
-
 import InstagramIcon from "@mui/icons-material/Instagram";
-import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 export default function Contact() {
   return (
     <div className="BScontactLinkContainer">
-    /*  <div className="BScontactLink">
-        <WhatsAppIcon sx={{ fontSize: "40px" }} />
-        <p>(81) 99999-9999</p>
-      </div>*/
-
       <div className="BScontactLink">
         <InstagramIcon sx={{ fontSize: "40px" }} />
         <a href="https://www.instagram.com/mhs_barbershop/" target="_blank">
