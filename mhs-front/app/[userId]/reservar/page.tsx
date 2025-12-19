254dcc3e8b4af452a0341a650965eac626563a97
Marcus Xavier <110700931+marcusvhx@users.noreply.github.com>
2025-12-19 17:43:58 -0300

    Delete mhs-front directory

--- mhs-front/app/[userId]/reservar/page.tsx
+++
@@ -1,7 +0,0 @@
-import MakeReserv from "@/components/makeReserv/MakeReserv";
-import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
-
-export default function MakeReservPage({params}:{params:Params}) {
-  
-  return <MakeReserv userId={params.userId} />;
-}
