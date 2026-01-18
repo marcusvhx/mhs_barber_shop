e638f700d9b76ba6f2b1c1479d21af01dfceca17
Marcus Xavier <110700931+marcusvhx@users.noreply.github.com>
2026-01-18 00:03:24 -0300

    Delete s directory

--- s/components/ui/AppointmentButton.tsx
+++
@@ -1,7 +0,0 @@
-export default function AppointmentButton({}: {}) {
-  return (
-    <button className="bg-primary rounded-full px-4 py-1 text-center text-background">
-      Fazer reserva
-    </button>
-  );
-}
--- s/components/ui/Title.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function Title({ children }: { children: ReactNode }) {
-  return (
-    <h1 className="flex text-lg font-bold border-b-2 border-primary px-5">
-      {children}
-    </h1>
-  );
-}
